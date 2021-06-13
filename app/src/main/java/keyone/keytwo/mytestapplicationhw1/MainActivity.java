5a502f31b40ab6499297cd033b2efc100cd738ce
Djirgal <djirgal.nemgirova@gmail.com>
2021-06-14 02:55:54 +0300

    Initial commit, 1st task of the HomeWork1 is done

---
+++ app/src/main/java/keyone/keytwo/mytestapplicationhw1/MainActivity.java
@@ -0,0 +1,14 @@
+package keyone.keytwo.mytestapplicationhw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);//вызов родительского класса
+        setContentView(R.layout.activity_main);//метод устанавливает содержимое activity из layout-файла
+    }
+}
